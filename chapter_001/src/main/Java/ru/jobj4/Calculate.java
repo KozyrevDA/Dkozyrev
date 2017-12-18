a08ed35f01193cdc050e0f8e2bf32dc2ed82e39d
KozyrevDA <garant-di@mail.ru>
2017-12-18 16:12:58 +0400

    1.3 JavaDoc. Code convention

---
+++ chapter_001/src/main/Java/ru/jobj4/Calculate.java
@@ -0,0 +1,18 @@
+/**
+* Package for calculate task.
+*
+* @author Kozyrev
+* @version $Id$
+* @since 0.1
+*/
+package DKozyrev;
+
+/**
+* Конструктор, вывод строки в консоль
+*/
+
+public class Calculate {
+	public static void main(String[] args){
+		System.out.println("Hello World.");
+	}
+}
